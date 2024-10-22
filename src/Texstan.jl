826231f100e594ba31db611f540800a5430df131
= <pjabardo@ipt.br>
2024-10-22 12:08:51 -0300

    Started wirking on the interface
    There are many capabilities for the progam and it is kind of confusing.
    I need to study other example cases more to get a better grip.

--- src/Texstan.jl
+++ src/Texstan.jl
@@ -1,5 +1,77 @@
 module Texstan
 
-# Write your package code here.
+export texstan, readtexstanfiles
+
+function texstan(exe, fname::AbstractString, dname)
+    d = pwd()
+    
+    exe1 = abspath(exe)
+    fname1 = abspath(fname)
+    
+    try
+        cd(dname)
+        if Sys.islinux() || Sys.isapple()
+            run(pipeline(`echo $fname1`, `wine $exe1`))
+        else
+            run(pipeline(`echo $fname1`, ` $exe1`))
+        end
+    finally
+        cd(d)
+    end
+    
+end
+
+function readtexstanfiles(fname)
+
+end
+
+
+
+abstract type AbstractTexstanGeometry end
+
+struct BoundaryLayer2d <: AbstractTexstanGeometry
+    kgeom::Int
+end
+
+struct BoundaryLayerRev <: AbstractTexstanGeometry
+    kgeom::Int
+end
+
+struct BoundaryLayerNozzle <: AbstractTexstanGeometry
+    kgeom::Int
+end
+
+struct PipeFlow <: AbstractTexstanGeometry
+    kgeom::Int
+end
+
+struct PlaneFlowSymm <: AbstractTexstanGeometry
+    kgeom::Int
+end
+
+struct PlaneFlow <: AbstractTexstanGeometry
+    kgeom::Int
+end
+
+struct AnnularDuct <: AbstractTexstanGeometry
+    kgeom::Int
+    ri::Float64
+    ro::Float64
+end
+
+
+kgeom(geo::AbstractTexstanGeometry) = geo.kgeom
+
+
+
+struct FluidProp
+    kfluid::Int
+        
+end
+
+
+isconstprop(fl::FluidProp) = fl.kfluid==1
+
+
 
 end
